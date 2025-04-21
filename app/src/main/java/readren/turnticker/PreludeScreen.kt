cad1f6d4f3bd26720729c1fb9df44e47abc79bc2
Guido Gustavo Pollitzer <gustavopollitzer@gmail.com>
2025-04-21 16:52:43 -0300

    Make player timer take all available vertical space or be scrollable if they don't fit.

--- app/src/main/java/readren/turnticker/PreludeScreen.kt
+++ app/src/main/java/readren/turnticker/PreludeScreen.kt
@@ -1,13 +1,8 @@
 package readren.turnticker
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
--- app/src/main/java/readren/turnticker/TimersScreen.kt
+++ app/src/main/java/readren/turnticker/TimersScreen.kt
@@ -1,15 +1,16 @@
 package readren.turnticker
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -23,14 +24,25 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import java.util.Locale
+
+private enum class FitState {
+	UNKNOWN, FIT, DO_NOT_FIT
+}
 
 @Preview
 @Composable
@@ -53,20 +65,58 @@ fun TimersScreen(appViewModel: AppViewModel = viewModel()) {
 		}
 		Section {
 			Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
-				Text("Participant", style = MaterialTheme.typography.headlineLarge, modifier = Modifier.padding(4.dp))
-				Text(appViewModel.viewMode.header, style = MaterialTheme.typography.headlineSmall, textAlign = TextAlign.Center, modifier = Modifier.padding(4.dp))
+				Text("Participant", style = MaterialTheme.typography.headlineLarge, modifier = Modifier.padding(horizontal = 4.dp))
+				Text(appViewModel.viewMode.header, style = MaterialTheme.typography.headlineSmall, textAlign = TextAlign.Center, modifier = Modifier.padding(horizontal = 4.dp))
 			}
-			Spacer(modifier = Modifier.width(8.dp))
-			Column(Modifier.verticalScroll(rememberScrollState())) {
-				appViewModel.getPlayers().forEach { player ->
-					key(player.name) {
-						if (player == appViewModel.selectedPlayer) {
-							SelectedPlayer()
-						} else {
-							UnselectedPlayer(player.name)
+			var fitState: FitState by remember { mutableStateOf(FitState.UNKNOWN) }
+
+			if (fitState == FitState.UNKNOWN || fitState == FitState.FIT) {
+				Layout(
+					appViewModel.getPlayers().map { player ->
+						@Composable {
+							if (player == appViewModel.selectedPlayer) {
+								SelectedPlayer()
+							} else {
+								UnselectedPlayer(player.name)
+							}
+						}
+					},
+				) { measurables, constraints ->
+					val playersMeasurables = measurables.map { it.first() }
+					val availableHeight = constraints.maxHeight
+					val minIntrinsicsHeights = playersMeasurables.map { it.minIntrinsicHeight(constraints.maxWidth) }
+					val totalMinIntrinsicHeight = minIntrinsicsHeights.sum()
+					fitState = if (availableHeight >= totalMinIntrinsicHeight) FitState.FIT else FitState.DO_NOT_FIT
+					val extraHeightPerPlayer = (constraints.maxHeight - totalMinIntrinsicHeight) / playersMeasurables.size
+					val playersPlaceables = minIntrinsicsHeights.zip(playersMeasurables) { minIntrinsicHeight, playerMeasurable ->
+						val height = if (extraHeightPerPlayer > 0) minIntrinsicHeight + extraHeightPerPlayer else minIntrinsicHeight
+						playerMeasurable.measure(constraints.copy(minHeight = height, maxHeight = height))
+					}
+					layout(constraints.maxWidth, constraints.maxHeight) {
+						var y = 0
+						playersPlaceables.forEach {
+							it.placeRelative(0, y)
+							y += it.height
 						}
 					}
 				}
+			} else {
+				Column(
+					Modifier
+						.verticalScroll(rememberScrollState())
+						.fillMaxHeight()
+				) {
+					appViewModel.getPlayers().forEach { player ->
+						key(player.name) {
+							if (player == appViewModel.selectedPlayer) {
+								SelectedPlayer()
+							} else {
+								UnselectedPlayer(player.name)
+							}
+						}
+					}
+				}
+
 			}
 		}
 	}
@@ -78,10 +128,14 @@ fun SelectedPlayer() {
 	val appViewModel: AppViewModel = viewModel()
 	appViewModel.selectedPlayer?.let { player ->
 		val timePresented = appViewModel.timePresentedFor(player)
-		Surface(color = MaterialTheme.colorScheme.primary, border = BorderStroke(2.dp, MaterialTheme.colorScheme.outline)) {
-			Column {
+		Surface(color = MaterialTheme.colorScheme.primary, border = BorderStroke(2.dp, MaterialTheme.colorScheme.outline), modifier = Modifier.clip(MaterialTheme.shapes.medium)) {
+			Column(verticalArrangement = Arrangement.SpaceAround) {
 				Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
-					Text(player.name, style = MaterialTheme.typography.displaySmall, modifier = Modifier.padding(start = 8.dp, end = 8.dp))
+					Text(
+						player.name, maxLines = 1, overflow = TextOverflow.Clip, style = MaterialTheme.typography.displaySmall, color = MaterialTheme.colorScheme.onPrimary, modifier = Modifier
+							.padding(start = 8.dp, end = 8.dp)
+							.weight(1f)
+					)
 					TimerDisplay(timePresented)
 				}
 				Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
@@ -106,20 +160,22 @@ fun UnselectedPlayer(playerName: String) {
 	appViewModel.getPlayer(playerName)?.let { player ->
 		val timePresented = appViewModel.timePresentedFor(player)
 		val allowsToFinishRound = player.allowsToFinishRound()
-		Surface(
-			color = if (allowsToFinishRound) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.primaryContainer,
-			border = BorderStroke(2.dp, MaterialTheme.colorScheme.outlineVariant)
+		Row(
+			horizontalArrangement = Arrangement.SpaceBetween,
+			verticalAlignment = Alignment.CenterVertically,
+			modifier = Modifier
+				.clip(MaterialTheme.shapes.medium)
+				.border(BorderStroke(2.dp, MaterialTheme.colorScheme.outlineVariant))
+				.background(if (allowsToFinishRound) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.primaryContainer)
+				.fillMaxWidth()
+				.clickable { appViewModel.changeSelectedPlayer(player) },
 		) {
-			Row(
-				horizontalArrangement = Arrangement.SpaceBetween,
-				verticalAlignment = Alignment.CenterVertically,
-				modifier = Modifier
-					.fillMaxWidth()
-					.clickable { appViewModel.changeSelectedPlayer(player) },
-			) {
-				Text(playerName, style = MaterialTheme.typography.displaySmall, modifier = Modifier.padding(start = 8.dp, end = 8.dp))
-				TimerDisplay(timePresented)
-			}
+			Text(
+				playerName, maxLines = 1, overflow = TextOverflow.Clip, style = MaterialTheme.typography.displaySmall, modifier = Modifier
+					.padding(start = 8.dp, end = 8.dp)
+					.weight(1f)
+			)
+			TimerDisplay(timePresented)
 		}
 	}
 }
